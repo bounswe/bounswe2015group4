99541bc8697ae6e0cf540fe7359c80861234aa50
Oğuz Erdoğmuş <oguzerd93@gmail.com>
2015-05-03 14:22:48 +0300

    Update MathematicalOperations.java
    binaryMinus function is added.

--- src/MathematicalOperations.java
+++ src/MathematicalOperations.java
@@ -5,4 +5,8 @@ public class MathematicalOperations {
     public int binaryPlus(int x, int y){
         return x + y;
     }
+    
+    public int binaryMinus(int x, int y) {
+        return x - y;
+    }
 }
