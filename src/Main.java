3f8cd5ea07cbe44522a1177dd57480a87de9a057
Mert Can Çüçen <mertcancucen@gmail.com>
2015-05-03 14:09:18 +0300

    Mathematical Operations

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by mertcan on 3.5.2015.
+ */
+public class Main {
+    public static void main (String[] args){
+        System.out.print("Hello World");
+    }
+}
---
+++ src/MathematicalOperations.java
@@ -0,0 +1,5 @@
+/**
+ * Created by mertcan on 3.5.2015.
+ */
+public class MathematicalOperations {
+}
