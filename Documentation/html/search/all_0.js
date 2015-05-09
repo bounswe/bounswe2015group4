3ea2d34dd1097818f57ffcf2d3eb8908e91028ef
ilkegultekin <gilkegultekin@yahoo.com>
2015-05-09 10:09:43 +0300

    Created a folder for doxygen documents, added the html files created by doxygen for the MathematicalOperations class.

---
+++ Documentation/html/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['binaryminus',['binaryMinus',['../class_mathematical_operations.html#af981d2acd45d0d746aff032f4592773a',1,'MathematicalOperations']]],
+  ['binarymultiplication',['binaryMultiplication',['../class_mathematical_operations.html#a2110b2924e6f0dd99f8bb9ef54e4f2c7',1,'MathematicalOperations']]],
+  ['binaryplus',['binaryPlus',['../class_mathematical_operations.html#a868e597064506a61471151a8eedcf545',1,'MathematicalOperations']]]
+];
---
+++ Documentation/html/search/functions_3.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['testbinaryminus',['testBinaryMinus',['../class_mathematical_operations_test.html#a3e9ba8627d2a676707e6659716445b2e',1,'MathematicalOperationsTest']]],
+  ['testbinarymultiplication',['testBinaryMultiplication',['../class_mathematical_operations_test.html#af45be66efa12a77788760afe6bd99657',1,'MathematicalOperationsTest']]],
+  ['testbinaryplus',['testBinaryPlus',['../class_mathematical_operations_test.html#ab08f798a266743439b41653ef7ca65b3',1,'MathematicalOperationsTest']]],
+  ['testisequal',['testIsEqual',['../class_mathematical_operations_test.html#a18cb9348273ece6b57b969f8c48633b3',1,'MathematicalOperationsTest']]],
+  ['testisgreater',['testIsGreater',['../class_mathematical_operations_test.html#aba1f9c2a9b911902241d4a1267b1e4a5',1,'MathematicalOperationsTest']]],
+  ['testissmaller',['testIsSmaller',['../class_mathematical_operations_test.html#a8aec0e5b82ee5a672f381bb710907b53',1,'MathematicalOperationsTest']]],
+  ['testremainder',['testRemainder',['../class_mathematical_operations_test.html#a57b26b0a0cf3908a927b384b1989e579',1,'MathematicalOperationsTest']]]
+];
