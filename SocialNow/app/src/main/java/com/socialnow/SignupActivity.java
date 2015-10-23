c3423aefe2aedafcd9a33089f6d29a0d634ababa
tszyanalau <amy.lau@boun.edu.tr>
2015-10-23 22:34:19 +0300

    Layouts of sign up page and email confirmation page created.

---
+++ SocialNow/app/src/main/java/com/socialnow/SignupActivity.java
@@ -0,0 +1,33 @@
+package com.socialnow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class SignupActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        spinner = (Spinner)findViewById(R.id.sUserType);
+        adapter = ArrayAdapter.createFromResource(this,R.array.user_type,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        /*spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+             Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + " selected", Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });*/
+    }
+}
