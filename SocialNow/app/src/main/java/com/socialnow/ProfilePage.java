fe55404090ab8c324b7246508692574ba446f75f
berfu <berfu.buyukoz@boun.edu.tr>
2015-10-31 21:27:09 +0200

    Android | ProfileFrag is connected to ProfilePage.

---
+++ SocialNow/app/src/main/java/com/socialnow/ProfilePage.java
@@ -0,0 +1,93 @@
+package com.socialnow;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import java.io.File;
+import android.widget.ImageView;
+
+public class ProfilePage extends AppCompatActivity {
+    Button logout;
+    ParseUser current_user = ParseUser.getCurrentUser();
+    String user_id= current_user.getObjectId();
+    ImageView pp_imageView;
+    private static final int SELECTED_PICTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+        pp_imageView = (ImageView)findViewById(R.id.profilepicture);
+
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        //intent.setType("image/*");
+        //intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,  SELECTED_PICTURE);
+    }
+
+
+    public void log_out(View v){
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            ParseUser.logOut();
+            Intent i2 = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(i2);
+        } else {
+            Log.d("error_logout", "logout failed");
+        }
+    }
+
+    public void onActivityResult(int reqCode, int resCode, Intent data)
+    {
+        super.onActivityResult(reqCode,resCode,data);
+
+        if(resCode == RESULT_OK)
+        {
+            if(reqCode == 1)
+            {
+                Uri uri = data.getData();
+                pp_imageView.setImageURI(data.getData());
+                String[] projection = {MediaStore.Images.Media.DATA};
+
+                Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+                cursor.moveToFirst();
+
+                int column_index = cursor.getColumnIndex(projection[0]);
+                String file_path = cursor.getString(column_index);
+                cursor.close();
+
+                Bitmap your_selected_image = BitmapFactory.decodeFile(file_path);
+                Drawable d = new BitmapDrawable(your_selected_image);
+                pp_imageView.setBackground(d);
+                current_user.put("profile_picture", data.getData().toString());
+
+
+            }
+        }
+    }
+
+
+}
+
+
