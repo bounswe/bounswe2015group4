269093c9738d76235fc6e7702a0236c1843e058f
Mert Can Çüçen <mertcancucen@gmail.com>
2015-11-24 14:39:25 +0200

    Small Updates for backend

--- src/SOCIALNOW/src/main/java/socialnow/Application.java
+++ src/SOCIALNOW/src/main/java/socialnow/Application.java
@@ -2,11 +2,18 @@ package socialnow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-  public static void main(String[] args) {
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
+  }
+
+  public static void main(String[] args) throws Exception {
     SpringApplication.run(Application.class, args);
   }
 
