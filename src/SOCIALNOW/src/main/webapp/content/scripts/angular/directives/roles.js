8b282662073bc9e98f2b4a161b421bec451d7363
Furkan Caymaz <furkan.caymaz@boun.edu.tr>
2015-12-31 13:49:26 +0200

    WEB |  Roles directive is created
    Roles directive will be used in privacy settings.

--- src/SOCIALNOW/src/main/webapp/content/scripts/angular/directives/roles.js
+++ src/SOCIALNOW/src/main/webapp/content/scripts/angular/directives/roles.js
@@ -3,7 +3,10 @@ app.directive('roles', function() {
         restrict: 'E',
         templateUrl: './content/scripts/angular/directives/partials/roles.html',
         link: function(scope, element, attrs, ctrl) {
-            scope.roles = ['Teaching Assistant', 'Student', 'Instructor', 'Alumni', 'All'];
+            scope.roles = [ { id: 1, name: 'Teaching Assistant' }, { id: 2, name: 'Student' }, { id: 3, name: 'Instructor' },
+                { id: 4, name: 'Alumni' }];
+
+            angular.element(document.getElementsByClassName("select2")).select2();
         }
     }
 })
