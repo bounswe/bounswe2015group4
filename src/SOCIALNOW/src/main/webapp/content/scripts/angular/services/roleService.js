806a2dbf5d6e5443a6cee36751a7a93ad26c6711
furkancaymaz <furkan.caymaz@boun.edu.tr>
2016-01-07 11:17:20 +0200

    WEB | Commenting angular codes continues v.2

--- src/SOCIALNOW/src/main/webapp/content/scripts/angular/services/roleService.js
+++ src/SOCIALNOW/src/main/webapp/content/scripts/angular/services/roleService.js
@@ -7,11 +7,21 @@
 app.service('roleService', ['$q', function($q) {
     var roles = [ { id: 1, name: 'Teaching Assistant' }, { id: 2, name: 'Undergraduate' }, { id: 3, name: 'Faculty' },
         { id: 4, name: 'Alumni' }];
-
+    /**
+    *In the function getRoles, all the roles defined in the system
+    *are returned.
+    *
+    *@return allTheRoles
+    */
     this.getRoles = function() {
         return roles;
     }
-
+    /**
+    *In the function getRoleIds, all the roles' ids defined in the system
+    *are returned.
+    *
+    *@return allTheRoleIds
+    */
     this.getRoleIds = function() {
         var ids = [];
 
@@ -21,7 +31,12 @@ app.service('roleService', ['$q', function($q) {
 
         return ids;
     }
-
+    /**
+    *In the function getRoleNamesAccordingToIds, the function takes an ids array as variable,
+    *and by considering the mapping in the array, returns the roles
+    *
+    *@return selectedRoles
+    */
     this.getRoleNamesAccordingToIds = function(ids) {
         if(_.isEqual(this.getRoleIds(), ids)) {
             return "all";
