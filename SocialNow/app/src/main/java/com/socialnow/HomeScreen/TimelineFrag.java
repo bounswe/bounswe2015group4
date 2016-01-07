e66cf0e80c8d022daa446c2705a16d4896369c72
Mert Can Çüçen <mertcancucen@gmail.com>
2016-01-07 11:12:05 +0200

    ANDROID | Timeline fix

--- SocialNow/app/src/main/java/com/socialnow/HomeScreen/TimelineFrag.java
+++ SocialNow/app/src/main/java/com/socialnow/HomeScreen/TimelineFrag.java
@@ -78,11 +78,11 @@ public class TimelineFrag extends Fragment {
         List<User_Group> ug = timeLine.getUser_groups();
         for(int i = 0; i < ue.size(); i++){
             User_Event t = ue.get(i);
-            results.add(new Basicgroup_event(t.getUser().getUser_photo(), t.getUser().getName() + " " + t.getUser().getSurname(), t.getEvent().getTitle()));
+            results.add(new Basicgroup_event(t.getUser().getUser_photo(), t.getUser().getName() + " " + t.getUser().getSurname(), t.getEvent().getTitle(), t.getUser().getUser_token().equals(t.getEvent().getEvent_host_token())));
         }
         for(int i = 0; i < ug.size(); i++){
             User_Group t = ug.get(i);
-            results.add(new Basicgroup_event(t.getUser().getUser_photo(), t.getUser().getName() + " " + t.getUser().getSurname(), t.getGroup().getGroup_name()));
+            results.add(new Basicgroup_event(t.getUser().getUser_photo(), t.getUser().getName() + " " + t.getUser().getSurname(), t.getGroup().getGroup_name(), t.getUser().getUser_token().equals(t.getGroup().getOwner_token())));
         }
     }
 
@@ -99,6 +99,13 @@ public class TimelineFrag extends Fragment {
             info.setText(results.get(position).owner + " ");
             TextView info2 = (TextView) v.findViewById(R.id.tInfoName);
             info2.setText(results.get(position).name);
+
+            String create_join = "joined";
+            if(!results.get(position).create_join){
+                TextView info3 = (TextView) v.findViewById(R.id.tInfocreated);
+                info3.setText(create_join);
+            }
+
 
             ImageView img = (ImageView) v.findViewById(R.id.ivEvent);
 //            Log.d("events", events.get(position).getEvent_photo() + " " + events.get(position).getEvent_description() + " " + events.get(position).getEvent_participant_users().length);
--- SocialNow/app/src/main/java/com/socialnow/Models/Basicgroup_event.java
+++ SocialNow/app/src/main/java/com/socialnow/Models/Basicgroup_event.java
@@ -7,10 +7,12 @@ public class Basicgroup_event {
     public String photoUrl;
     public String owner;
     public String name;
-    public Basicgroup_event(String photoUrl, String owner, String name){
+    public boolean create_join = true;
+    public Basicgroup_event(String photoUrl, String owner, String name, boolean create_join){
         this.photoUrl = photoUrl;
         this.owner = owner;
         this.name = name;
+        this.create_join = create_join;
     }
 
 }
