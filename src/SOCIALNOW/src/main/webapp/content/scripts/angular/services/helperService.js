2708a54c3ed83deffdce5803f4bdc0f0c37fb3ab
furkancaymaz <furkan.caymaz@boun.edu.tr>
2016-01-07 10:57:39 +0200

    WEB | Commenting angular codes continues

--- src/SOCIALNOW/src/main/webapp/content/scripts/angular/services/helperService.js
+++ src/SOCIALNOW/src/main/webapp/content/scripts/angular/services/helperService.js
@@ -1,16 +1,42 @@
 app.service('helperService', function($location, $log, $route) {
+    /**
+    *In the function goToPage, url is taken as variable of the function
+    *and in a case the function is successful, users are succesfuly directed to 
+    *pages defined in the url.
+    *
+    *@param url
+    *@return page
+    */
     this.goToPage = function(url) {
         $location.path(url);
     }
-
+    /**
+    *In the function consoleError, a message is taken as variable of the function
+    *and in a case the function is successful, the function writes an error message
+    *to the console.
+    *
+    *@param message
+    *@return errorMessage
+    */
     this.consoleError = function(message) {
         $log.error(message);
     }
-
+    /**
+    *In the function consoleInfo, a message is taken as variable of the function
+    *and in a case the function is successful, the function writes an info message
+    *to the console.
+    *
+    *@param message
+    *@return infoMessage
+    */
     this.consoleInfo = function(message) {
         $log.info(message);
     }
-
+    /**
+    *In the function reload, the page user currently using is refreshed.
+    *
+    *@return refreshedPage
+    */
     this.reload = function(){
         $route.reload();
     }
--- src/SOCIALNOW/src/main/webapp/content/scripts/angular/services/postService.js
+++ src/SOCIALNOW/src/main/webapp/content/scripts/angular/services/postService.js
@@ -1,5 +1,13 @@
 app.service('postService', function($q, $http, baseApiUrl) {
-
+    /**
+    *In the function createPost, post message and token are taken as variable of the function
+    *and in a case the function is successful, the function lets users to post a message under 
+    *a group or event. For all message posting requirements, this function is called.
+    *
+    *@param post
+    *@param token
+    *@return postedMessage
+    */
     this.createPost = function(post, token) {
         var deferred = $q.defer();
 
@@ -21,7 +29,15 @@ app.service('postService', function($q, $http, baseApiUrl) {
 
         return deferred.promise;
     }
-
+    /**
+    *In the function addPostToEvent, postId and eventId are taken as variable of the function
+    *and it aims to attach the created post in the database to an event by considering event's
+    *eventId.
+    *
+    *@param postId
+    *@param eventId
+    *@return eventPost
+    */
     this.addPostToEvent = function(postId, eventId) {
         var deferred = $q.defer();
 
@@ -42,7 +58,15 @@ app.service('postService', function($q, $http, baseApiUrl) {
 
         return deferred.promise;
     }
-
+    /**
+    *In the function addPostToGroup, postId and groupId are taken as variable of the function
+    *and it aims to attach the created post in the database to a group by considering group's
+    *groupId.
+    *
+    *@param postId
+    *@param groupId
+    *@return groupPost
+    */
     this.addPostToGroup = function(postId, groupId) {
         var deferred = $q.defer();
 
@@ -63,7 +87,15 @@ app.service('postService', function($q, $http, baseApiUrl) {
 
         return deferred.promise;
     }
-
+    /**
+    *In the function addComment, ownerToken and comment are taken as variable of the function
+    *and in a case the function is successful, the function lets users to create a comment. 
+    *To comment under any post in event or group, this function is called.
+    *
+    *@param comment
+    *@param ownerToken
+    *@return createdComment
+    */
     this.addComment = function(ownerToken, comment) {
         var deferred = $q.defer();
 
@@ -84,7 +116,15 @@ app.service('postService', function($q, $http, baseApiUrl) {
 
         return deferred.promise;
     }
-
+    /**
+    *In the function addCommentToPost, postId and commentId are taken as variable of the function
+    *and it aims to attach the created comment in the database to a post by considering post's
+    *postId.
+    *
+    *@param postId
+    *@param commentId
+    *@return commentForPost
+    */
     this.addCommentToPost = function(postId, commentId) {
         var deferred = $q.defer();
 
